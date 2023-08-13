da45a1feef950385037f430272abfd50881b65cd
nanamitoshimasu <toshimasunanami@toshimananakainoMacBook-Air.local>
2023-08-14 00:11:16 +0900

    layout done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 questions = [
-  ["高校の時の所属部活はなんでしょう？", "水泳部", "ラクロス部", "帰宅部"],
-  ["短大の時にしたハロウィンの仮装はなんでしょう？", "マリリン・マンソン", "バニーガール", "毛利蘭"],
+  ["高校生の時の所属部活はなんでしょう？", "水泳部", "ラクロス部", "帰宅部"],
+  ["短大生の時にしたハロウィンの仮装はなんでしょう？", "マリリン・マンソン", "バニーガール", "毛利蘭"],
   ["狂ったように好きだった歌手は誰でしょう？", "東方神起", "Hey!Say!JUMP", "EXILE"],
   ["明日の天気は？", "はれ", "あめ", "くもり"],
   ["学生時代好きだった人との結末は？", "そんなものない", "今も…♡", "甘酸っぱいお別れ"]
